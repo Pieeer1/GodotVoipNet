eff445b11645a750949406eeb5bc143715154c24
Pieeer1 <mason.adams@msn.com>
2023-12-14 12:59:59 -0800

    Initial Commit with Functionality and Icons

---
+++ addons/GodotVoipNet/Scripts/VoiceDataEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using Godot.Collections;
+using System;
+
+namespace Trouble.Audio;
+public class VoiceDataEventArgs : EventArgs
+{
+    public Array<float> Data { get; }
+    public int Id { get; }
+
+    public VoiceDataEventArgs(Array<float> data, int id)
+    {
+        Data = data;
+        Id = id;
+    }
+}
