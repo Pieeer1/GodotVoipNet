3a513bd4bb7df34a7d92f4b195e999b12c432dac
Mason Adams <mason.adams@msn.com>
2024-02-26 20:20:52 -0800

    Stereo Audio, Cleanup, Massive Performance Fix, Compilation Clear (#9)
    * initial setup, needs coming back to
    
    * ui setup.
    
    * adding voice button
    
    * fixing capatilization and some errors
    
    * more fixes
    
    * i fcuking give up for now idk why it doesn't work anymore
    
    * holy fucking shit it was the camera all along
    
    * dotnet 8 standard
    
    * docs
    
    * finally
    
    * finally
    
    * adding some more docs.

---
+++ Example/Startup.cs
@@ -0,0 +1,185 @@
+using Godot;
+using System;
+using System.Linq;
+
+public partial class Startup : Node
+{
+    [Export]
+    public int MaxPlayers { get; set; } = 4;
+    [Export]
+    public int ServerPort { get; set; } = 1234;
+
+    private readonly PackedScene _speakerScene = ResourceLoader.Load<PackedScene>("res://Example/Scenes/Speaker.tscn");
+
+    private Button _hostButton = null!;
+    private Button _joinButton = null!;
+    private Button _leaveButton = null!;
+    private Button _quitButton = null!;
+    private Node _speakerHolder = null!;
+    private CheckBox _pushToTalkCheckbox = null!;
+    private CheckBox _stereoCheckbox = null!;
+    private Label _tooltipLabel = null!;
+    private VBoxContainer _speakerUIHolder = null!;
+    private bool _isInSession = false;
+    private bool _isStereo = false;
+    private bool _isPushToTalk = false;
+    private int _id = 0;
+    public override void _Ready()
+    {
+        _hostButton = GetNode<Button>("Control/VBoxContainer/HBoxContainer/HostButton");
+        _joinButton = GetNode<Button>("Control/VBoxContainer/HBoxContainer/JoinButton");
+        _leaveButton = GetNode<Button>("Control/VBoxContainer/HBoxContainer/LeaveButton");
+        _quitButton = GetNode<Button>("Control/VBoxContainer/HBoxContainer/QuitButton");
+        _speakerHolder = GetNode<Node3D>("SpeakerHolder");
+        _pushToTalkCheckbox = GetNode<CheckBox>("Control/VBoxContainer/CheckboxContainer/PushToTalkCheckbox");
+        _stereoCheckbox = GetNode<CheckBox>("Control/VBoxContainer/CheckboxContainer/StereoCheckbox");
+        _tooltipLabel = GetNode<Label>("Control/VBoxContainer/TooltipLabel");
+        _speakerUIHolder = GetNode<VBoxContainer>("Control/VBoxContainer/SpeakerContainer/SpeakerHolder");
+
+        _hostButton.Pressed += HostButtonPressed;
+        _joinButton.Pressed += JoinButtonPressed;
+        _leaveButton.Pressed += LeaveButtonPressed;
+        _quitButton.Pressed += QuitButtonPressed;
+
+        _stereoCheckbox.Toggled += (bool isPressed) => _isStereo = isPressed;
+        _pushToTalkCheckbox.Toggled += (bool isPressed) =>
+        {
+            _isPushToTalk = isPressed;
+            if (!_isPushToTalk)
+            {
+                _speakerHolder.GetChildren().Cast<Speaker>().First(x => x.Name == $"{_id}").StartSpeaking();
+            }
+            else
+            {
+                _speakerHolder.GetChildren().Cast<Speaker>().FirstOrDefault(x => x.Name == $"{_id}")?.StopSpeaking();
+            }
+        };
+        //set defaults programatically for now
+        _pushToTalkCheckbox.ButtonPressed = true;
+        _isPushToTalk = true;
+    }
+
+    public override void _Process(double delta)
+    {
+        HandleButtonAvailability();
+        if (_isPushToTalk)
+        {
+            if (Input.IsActionPressed("voice_input"))
+            {
+                _speakerHolder.GetChildren().Cast<Speaker>().FirstOrDefault(x => x.Name == $"{_id}")?.StartSpeaking();
+            }
+            else
+            {
+                _speakerHolder.GetChildren().Cast<Speaker>().FirstOrDefault(x => x.Name == $"{_id}")?.StopSpeaking();
+            }
+        }
+    }
+
+    [Rpc(CallLocal = true)]
+    public void RefreshSpeakerList(int id)
+    {
+        ClearSpeakerList();
+
+        foreach (Node speaker in _speakerHolder.GetChildren().Where(x => x.Name != id.ToString()))
+        {
+            Label label = new Label();
+            label.Text = $"Speaker {speaker.Name}";
+            _speakerUIHolder.AddChild(label);
+        }
+    }
+    [Rpc(CallLocal = true)]
+    public void RefreshSpeakerList()
+    {
+        ClearSpeakerList();
+
+        foreach (Node speaker in _speakerHolder.GetChildren())
+        {
+            Label label = new Label();
+            label.Text = $"Speaker {speaker.Name}";
+            _speakerUIHolder.AddChild(label);
+        }
+    }
+    [Rpc]
+    public void SetId(int id)
+    {
+        if (_id == 0)
+        {
+            _id = id;
+        }
+    }
+    private void HandleButtonAvailability()
+    {
+        _leaveButton.Disabled = !_isInSession;
+        _joinButton.Disabled = _isInSession;
+        _hostButton.Disabled = _isInSession;
+        _pushToTalkCheckbox.Disabled = !_isInSession;
+        _stereoCheckbox.Disabled = !_isInSession;
+        _tooltipLabel.Visible = _isInSession && _isPushToTalk;
+    }
+
+    private void QuitButtonPressed()
+    {
+        GetTree().Quit();
+    }
+    private void ClearSpeakerList()
+    {
+        foreach (var child in _speakerUIHolder.GetChildren())
+        {
+            child.QueueFree();
+        }
+    }
+
+    private void LeaveButtonPressed()
+    {
+        _isInSession = false;
+
+        ClearSpeakerList();
+
+        if (Multiplayer.IsServer())
+        {
+            RemovePlayer(1);
+            Multiplayer.PeerConnected -= AddPlayer;
+            Multiplayer.PeerDisconnected -= RemovePlayer;
+        }
+
+        Multiplayer.MultiplayerPeer.Close();
+    }
+
+    private void JoinButtonPressed()
+    {
+        _isInSession = true;
+
+        ENetMultiplayerPeer peer = new ENetMultiplayerPeer();
+        peer.CreateClient("127.0.0.1", ServerPort);
+        Multiplayer.MultiplayerPeer = peer;
+    }
+
+    private void HostButtonPressed()
+    {
+        _isInSession = true;
+
+        ENetMultiplayerPeer peer = new ENetMultiplayerPeer();
+        peer.CreateServer(ServerPort, MaxPlayers);
+        Multiplayer.MultiplayerPeer = peer;
+
+        Multiplayer.PeerConnected += AddPlayer;
+        Multiplayer.PeerDisconnected += RemovePlayer;
+        _id = 1;
+        AddPlayer(1); // multiplayer always starts with id of 1
+    }
+
+    private void AddPlayer(long id)
+    {
+        Speaker speaker = _speakerScene.Instantiate<Speaker>();
+        speaker.Name = $"{id}";
+        speaker.Position = Vector3.Zero;
+        _speakerHolder.AddChild(speaker, true);
+        Rpc(nameof(RefreshSpeakerList));
+        Rpc(nameof(SetId), id);
+    }
+    private void RemovePlayer(long id) 
+    {
+        _speakerHolder.GetNode<Speaker>(id.ToString()).QueueFree();
+        Rpc(nameof(RefreshSpeakerList), id);
+    }
+}
