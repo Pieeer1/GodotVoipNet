ccce301c10d78fd65f13e282d57d495ad64a8074
Pieeer1 <mason.adams@msn.com>
2024-02-28 07:11:48 -0800

    cleanup some test settings.

---
+++ Tests/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using GdUnit4;
+using static GdUnit4.Assertions;
+
+namespace GodotVoipNet.Tests;
+
+[TestSuite]
+public class HelloWorld
+{
+    [TestCase]
+    public void Test()
+    {
+        AssertBool(true).IsTrue();
+    }
+}
